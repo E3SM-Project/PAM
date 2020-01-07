3b1a6e61e8b019c0f553bc1760a528c59bddb6ab
Matt Norman <normanmr@ornl.gov>
2020-01-07 17:05:02 -0500

    improving th eformat of the unit test output

--- dynamics/sam++/unit/unit_abcoefs.cpp
+++ dynamics/sam++/unit/unit_abcoefs.cpp
@@ -11,19 +11,17 @@ int main() {
 
   nstep = 1;
 
-  std::cout << "Got here 0" << std::endl;
-
   dt3(0) = 4.1;
-  dt3(1) = 4.1;
-  dt3(2) = 4.1;
+  dt3(1) = 4.2;
+  dt3(2) = 4.3;
+
+  std::cout << std::scientific;
 
   // Test 1
   na = 1 ; nb = 2 ; nc = 3;
-  std::cout << "Got here 1" << std::endl;
   abcoefs    (dt3       , nstep, na, nb, nc, at    , bt    , ct    );
-  std::cout << "Got here 2" << std::endl;
   abcoefs_f90(dt3.data(), nstep, na, nb, nc, at_f90, bt_f90, ct_f90);
-  std::cout << "abcoefs Test 1\n";
+  std::cout << "abcoefs Test 1: diff, c++, fortran\n";
   std::cout << "at: " << fabs(at-at_f90) << " " << at << " " << at_f90 << "\n";
   std::cout << "bt: " << fabs(bt-bt_f90) << " " << bt << " " << bt_f90 << "\n";
   std::cout << "ct: " << fabs(ct-ct_f90) << " " << ct << " " << ct_f90 << "\n\n";
@@ -32,7 +30,7 @@ int main() {
   na = 3 ; nb = 1 ; nc = 2;
   abcoefs    (dt3       , nstep, na, nb, nc, at    , bt    , ct    );
   abcoefs_f90(dt3.data(), nstep, na, nb, nc, at_f90, bt_f90, ct_f90);
-  std::cout << "abcoefs Test 2\n";
+  std::cout << "abcoefs Test 2: diff, c++, fortran\n";
   std::cout << "at: " << fabs(at-at_f90) << " " << at << " " << at_f90 << "\n";
   std::cout << "bt: " << fabs(bt-bt_f90) << " " << bt << " " << bt_f90 << "\n";
   std::cout << "ct: " << fabs(ct-ct_f90) << " " << ct << " " << ct_f90 << "\n\n";
@@ -41,7 +39,7 @@ int main() {
   na = 2 ; nb = 3 ; nc = 1;
   abcoefs    (dt3       , nstep, na, nb, nc, at    , bt    , ct    );
   abcoefs_f90(dt3.data(), nstep, na, nb, nc, at_f90, bt_f90, ct_f90);
-  std::cout << "abcoefs Test 3\n";
+  std::cout << "abcoefs Test 3: diff, c++, fortran\n";
   std::cout << "at: " << fabs(at-at_f90) << " " << at << " " << at_f90 << "\n";
   std::cout << "bt: " << fabs(bt-bt_f90) << " " << bt << " " << bt_f90 << "\n";
   std::cout << "ct: " << fabs(ct-ct_f90) << " " << ct << " " << ct_f90 << "\n\n";
@@ -52,7 +50,7 @@ int main() {
   na = 1 ; nb = 2 ; nc = 3;
   abcoefs    (dt3       , nstep, na, nb, nc, at    , bt    , ct    );
   abcoefs_f90(dt3.data(), nstep, na, nb, nc, at_f90, bt_f90, ct_f90);
-  std::cout << "abcoefs Test 4\n";
+  std::cout << "abcoefs Test 4: diff, c++, fortran\n";
   std::cout << "at: " << fabs(at-at_f90) << " " << at << " " << at_f90 << "\n";
   std::cout << "bt: " << fabs(bt-bt_f90) << " " << bt << " " << bt_f90 << "\n";
   std::cout << "ct: " << fabs(ct-ct_f90) << " " << ct << " " << ct_f90 << "\n\n";
@@ -61,7 +59,7 @@ int main() {
   na = 3 ; nb = 1 ; nc = 2;
   abcoefs    (dt3       , nstep, na, nb, nc, at    , bt    , ct    );
   abcoefs_f90(dt3.data(), nstep, na, nb, nc, at_f90, bt_f90, ct_f90);
-  std::cout << "abcoefs Test 5\n";
+  std::cout << "abcoefs Test 5: diff, c++, fortran\n";
   std::cout << "at: " << fabs(at-at_f90) << " " << at << " " << at_f90 << "\n";
   std::cout << "bt: " << fabs(bt-bt_f90) << " " << bt << " " << bt_f90 << "\n";
   std::cout << "ct: " << fabs(ct-ct_f90) << " " << ct << " " << ct_f90 << "\n\n";
@@ -70,7 +68,7 @@ int main() {
   na = 2 ; nb = 3 ; nc = 1;
   abcoefs    (dt3       , nstep, na, nb, nc, at    , bt    , ct    );
   abcoefs_f90(dt3.data(), nstep, na, nb, nc, at_f90, bt_f90, ct_f90);
-  std::cout << "abcoefs Test 6\n";
+  std::cout << "abcoefs Test 6: diff, c++, fortran\n";
   std::cout << "at: " << fabs(at-at_f90) << " " << at << " " << at_f90 << "\n";
   std::cout << "bt: " << fabs(bt-bt_f90) << " " << bt << " " << bt_f90 << "\n";
   std::cout << "ct: " << fabs(ct-ct_f90) << " " << ct << " " << ct_f90 << "\n\n";
@@ -81,7 +79,7 @@ int main() {
   na = 1 ; nb = 2 ; nc = 3;
   abcoefs    (dt3       , nstep, na, nb, nc, at    , bt    , ct    );
   abcoefs_f90(dt3.data(), nstep, na, nb, nc, at_f90, bt_f90, ct_f90);
-  std::cout << "abcoefs Test 7\n";
+  std::cout << "abcoefs Test 7: diff, c++, fortran\n";
   std::cout << "at: " << fabs(at-at_f90) << " " << at << " " << at_f90 << "\n";
   std::cout << "bt: " << fabs(bt-bt_f90) << " " << bt << " " << bt_f90 << "\n";
   std::cout << "ct: " << fabs(ct-ct_f90) << " " << ct << " " << ct_f90 << "\n\n";
@@ -90,7 +88,7 @@ int main() {
   na = 3 ; nb = 1 ; nc = 2;
   abcoefs    (dt3       , nstep, na, nb, nc, at    , bt    , ct    );
   abcoefs_f90(dt3.data(), nstep, na, nb, nc, at_f90, bt_f90, ct_f90);
-  std::cout << "abcoefs Test 8\n";
+  std::cout << "abcoefs Test 8: diff, c++, fortran\n";
   std::cout << "at: " << fabs(at-at_f90) << " " << at << " " << at_f90 << "\n";
   std::cout << "bt: " << fabs(bt-bt_f90) << " " << bt << " " << bt_f90 << "\n";
   std::cout << "ct: " << fabs(ct-ct_f90) << " " << ct << " " << ct_f90 << "\n\n";
@@ -99,7 +97,7 @@ int main() {
   na = 2 ; nb = 3 ; nc = 1;
   abcoefs    (dt3       , nstep, na, nb, nc, at    , bt    , ct    );
   abcoefs_f90(dt3.data(), nstep, na, nb, nc, at_f90, bt_f90, ct_f90);
-  std::cout << "abcoefs Test 9\n";
+  std::cout << "abcoefs Test 9: diff, c++, fortran\n";
   std::cout << "at: " << fabs(at-at_f90) << " " << at << " " << at_f90 << "\n";
   std::cout << "bt: " << fabs(bt-bt_f90) << " " << bt << " " << bt_f90 << "\n";
   std::cout << "ct: " << fabs(ct-ct_f90) << " " << ct << " " << ct_f90 << "\n\n";
