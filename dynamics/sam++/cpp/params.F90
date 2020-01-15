5a8c227950202cae04f2ef54c879f6574d10105c
Matt Norman <normanmr@ornl.gov>
2020-01-15 11:49:12 -0500

    Forgot to make param.F90 use c_double for r8 and crm_rknd

--- dynamics/sam++/cpp/params.F90
+++ dynamics/sam++/cpp/params.F90
@@ -9,8 +9,8 @@ module params
 !  shr_const_rhofw
 !#endif /*CRM*/
   implicit none
-  integer, parameter :: r8 = selected_real_kind(13)
-  integer, parameter :: crm_rknd = selected_real_kind(13)
+  integer, parameter :: r8 = c_double
+  integer, parameter :: crm_rknd = c_double
   integer, parameter :: crm_iknd = c_int
   integer, parameter :: crm_lknd = c_bool
   !   Constants:
