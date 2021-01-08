e1b51221c7fdf24373900dfc65a270ee7c1ee1ca
Matt Norman <normanmr@ornl.gov>
2021-01-08 09:37:54 -0500

    Improvements to vertical level generation script

--- dynamics/awfl/generate_vertical_levels.py
+++ dynamics/awfl/generate_vertical_levels.py
@@ -4,7 +4,7 @@ import sys, getopt
 import matplotlib.pyplot as plt
 from netCDF4 import Dataset
 
-avail = ["help","function=","exp-base=","tanh-inflect=","tanh-steep=","tanh-scale=","z0=","ztop=","nlev=","no-plots","output="]
+avail = ["help","function=","exp-base=","tanh-inflect=","tanh-steep=","tanh-scale=","z0=","ztop=","nlev=","show-plots","output="]
 
 function="tanh"
 tanh_inflect=0.4
@@ -13,21 +13,23 @@ tanh_scale=10.
 exp_base=10.
 z0=0.
 ztop=10000.
-nlev=128
-showplots=True
+nlev=64
+showplots=False
 output="vcoords.nc"
 
 def print_help() :
-  print("Usage: python generate_vertical_levels.py")
+  print("Usage: python generate_vertical_levels.py [...]")
   print("Options:")
-  print("--function=[exp|tanh]")
+  print("--function=[equal|exp|tanh]")
   print("  equal: self-explanatory")
   print("  exp:")
   print("    * This gives you a consistent coarsening as you increase in height")
+  print("    * dz(k) = exp( pow( exp_base , k/(nlev-1) ) )  for k in {0,...,nlev-1}")
   print("    --exp-base=10")
   print("      * Top cell grid spacing will be 10x larger than bottom cell")
   print("  tanh:")
   print("    * This allows you to concentrate layers at the surface, then keep grid spacing fairly constant after that")
+  print("    * dz(k) = ( ( tanh( (k/(nlev-1)-tanh_inflect)*tanh_steep ) + 1 ) / 2 * (tanh_scale-1) ) + 1  for k in {0,...,nlev-1}")
   print("    --tanh-inflect=0.4")
   print("      * tanh inflection point is 40% through the vertical indices")
   print("    --tanh-steep=5")
@@ -37,23 +39,22 @@ def print_help() :
   print("      * The maximum bound on the grid spacing ratio of top cell to bottom cell")
   print("--help")
   print("  * Print this message")
-  print("--z0=[Bottom of the vertical grid]")
-  print("--ztop=[Top of the vertical grid]")
-  print("--nlev=[Number of cells]")
-  print("--no-plots")
-  print("  * Don't show matplotlib plots, e.g., over a remote server w/ a laggy connection")
-  print("--output=[vcoords.nc]")
+  print("--z0=0        [Bottom of the vertical grid]")
+  print("--ztop=10000  [Top of the vertical grid]")
+  print("--nlev=64     [Number of vertical cells]")
+  print("--show-plots")
+  print("  * Show matplotlib plots over X11")
+  print("--output=vcoords.nc  [NetCDF filename to save the coordinates to]")
 
+print_help()
 
 try:
   opts , args = getopt.getopt(sys.argv[1:] , "h" , avail)
 except:
   print("ERROR: Invalid argument")
-  print_help()
   sys.exit(-1)
 for opt,arg in opts:
   if (opt == "-h" or opt == "--help") :
-    print_help()
     sys.exit()
   elif opt == "--function" :
     function = arg
@@ -71,16 +72,16 @@ for opt,arg in opts:
     ztop = float(arg)
   elif opt == "--nlev" :
     nlev = int(arg)
-  elif opt == "--no-plots" :
-    showplots = False
+  elif opt == "--show-plots" :
+    showplots = True
   elif opt == "--output" :
     output = arg
   else :
     print("ERROR: Invalid argument")
-    # print_help()
     sys.exit(-1)
     
 
+print("\n***** USING THE FOLLOWING PARAMETERS *****")
 print("nlev: "+str(nlev))
 print("z0: "+str(z0))
 print("ztop: "+str(ztop))
@@ -118,23 +119,28 @@ for i in range(nlev) :
   dz[i] = zint[i+1] - zint[i]
   zmid[i] = ( zint[i+1] + zint[i] ) / 2
 
+plt.plot(range(nlev),dz)
+plt.xlabel("vertical level index")
+plt.ylabel("grid spacing")
+plt.savefig("grid_spacing_vs_level_index.png")
 if showplots :
-  plt.plot(range(nlev),dz)
-  plt.xlabel("vertical level index")
-  plt.ylabel("grid spacing")
   plt.show()
 
-  plt.close()
+plt.close()
 
-  skip = int( nlev / 50 )
-  plt.hlines(zint[::skip],0,1,linewidth=0.2)
-  plt.ylabel("height")
+skip = int( nlev / 50 )
+plt.hlines(zint[::skip],0,1,linewidth=0.2)
+plt.ylabel("height")
+plt.savefig("grid_spacing_visual.png",dpi=600)
+if showplots :
   plt.show()
 
 print("\n**************** grid spacing ****************")
 print(dz)
 print("\n**************** interface locations ****************")
 print(zint)
+
+print("\nFigures saved in grid_spacing_vs_level_index.png and grid_spacing_visual.png")
 
 
 nc = Dataset(output,"w")
