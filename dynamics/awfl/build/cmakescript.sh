21f39258824fbfa56739f10e27d4a53f314b305c
Matt Norman <normanmr@ornl.gov>
2021-02-09 11:29:16 -0500

    Using some conveniences in an update YAKL. Tryign to get things working on Summit

--- dynamics/awfl/build/cmakescript.sh
+++ dynamics/awfl/build/cmakescript.sh
@@ -5,7 +5,6 @@
 cmake      \
   -DCMAKE_CXX_FLAGS="${CXXFLAGS}"   \
   -DNCFLAGS="${NCFLAGS}"            \
-  -DYAKL_CUB_HOME="${CUB_HOME}"     \
   -DYAKL_HOME="${YAKL_HOME}"        \
   -DARCH="${ARCH}"                  \
   ..
