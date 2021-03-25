5e35d1dc07001bdabe51efe2ab3c8fb5d0affc27
Matt Norman <normanmr@ornl.gov>
2021-03-25 16:10:29 -0400

    I can't seem to get any more information out of burger's regarding oscillations and implementation choices to resolve the 3-D Euler instabilities.

--- dynamics/awfl/unit/burger_prim/burger_prim.cpp
+++ dynamics/awfl/unit/burger_prim/burger_prim.cpp
@@ -4,7 +4,8 @@
 
 
 real func(real xloc) {
-  return (cos(2*M_PI*xloc) + 1.5) / 2;
+  // return (cos(2*M_PI*xloc) + 1.5) / 2;
+  return xloc > 0.4 && xloc < 0.6 ? 1 : 0;
 }
 
 
@@ -55,9 +56,25 @@ void print_state( realHost1d const &state ) {
 
 
 
+void print_states( realHost1d const &state1 , realHost1d const &state2 ) {
+  for (int i=0; i < state1.dimension[0]; i++) {
+    std::cout << std::scientific << state1(i) << "  " << state2(i) << "\n";
+  }
+}
+
+
+
+void print_states( realHost1d const &state1 , realHost1d const &state2 , realHost1d const &state3 ) {
+  for (int i=0; i < state1.dimension[0]; i++) {
+    std::cout << std::scientific << state1(i) << "  " << state2(i) << "  " << state3(i) << "\n";
+  }
+}
+
+
+
 // Conservative approach
 template <int ord>
-realHost1d simulate_cons( int nx , real sim_time ) {
+realHost1d simulate_cons( int nx , real sim_time , bool weno ) {
   int constexpr hs = (ord-1)/2;
 
   // Setup transformation matrices and WENO
@@ -75,6 +92,13 @@ realHost1d simulate_cons( int nx , real sim_time ) {
 
   TransformMatrices::get_gll_points (gllPts_ord);
   TransformMatrices::get_gll_weights(gllWts_ord);
+
+  SArray<real,3,ord,ord,ord> wenoRecon;
+  SArray<real,1,hs+2> idl;
+  real sigma;
+
+  TransformMatrices::weno_sten_to_coefs(wenoRecon);
+  weno::wenoSetIdealSigma<ord>(idl,sigma);
 
   real dx = 1./nx;
   real dt = 0.4 * dx / 1.25;
@@ -103,7 +127,14 @@ realHost1d simulate_cons( int nx , real sim_time ) {
       SArray<real,1,ord> stencil;
       for (int ii=0; ii < ord; ii++) { stencil(ii) = state(i+ii); }
       // Reconstruct
-      SArray<real,1,ord> gll = c2g * s2c * stencil;
+      SArray<real,1,ord> gll;
+      if (weno) {
+        SArray<real,1,ord> wenoCoefs;
+        weno::compute_weno_coefs<ord>( wenoRecon , stencil , wenoCoefs , idl , sigma );
+        gll = c2g * wenoCoefs;
+      } else {
+        gll = c2g * s2c * stencil;
+      }
       SArray<real,2,ord,ord> state_DTs;
       SArray<real,2,ord,ord> flux_DTs;
       for (int ii=0; ii < ord; ii++) {
@@ -150,7 +181,7 @@ realHost1d simulate_cons( int nx , real sim_time ) {
 // The most accurate primitive equation approach
 // Conservative DTs, then exact central integral, then exact fwaves
 template <int ord>
-realHost1d simulate_prim1( int nx , real sim_time ) {
+realHost1d simulate_prim1( int nx , real sim_time , bool weno ) {
   int constexpr hs = (ord-1)/2;
 
   // Setup transformation matrices and WENO
@@ -168,6 +199,13 @@ realHost1d simulate_prim1( int nx , real sim_time ) {
 
   TransformMatrices::get_gll_points (gllPts_ord);
   TransformMatrices::get_gll_weights(gllWts_ord);
+
+  SArray<real,3,ord,ord,ord> wenoRecon;
+  SArray<real,1,hs+2> idl;
+  real sigma;
+
+  TransformMatrices::weno_sten_to_coefs(wenoRecon);
+  weno::wenoSetIdealSigma<ord>(idl,sigma);
 
   real dx = 1./nx;
   real dt = 0.4 * dx / 1.25;
@@ -197,8 +235,17 @@ realHost1d simulate_prim1( int nx , real sim_time ) {
       SArray<real,1,ord> stencil;
       for (int ii=0; ii < ord; ii++) { stencil(ii) = state(i+ii); }
       // Reconstruct
-      SArray<real,1,ord> u  = c2g       * s2c * stencil;
-      SArray<real,1,ord> du = c2g * c2d * s2c * stencil;
+      SArray<real,1,ord> u ;
+      SArray<real,1,ord> du;
+      if (weno) {
+        SArray<real,1,ord> wenoCoefs;
+        weno::compute_weno_coefs<ord>( wenoRecon , stencil , wenoCoefs , idl , sigma );
+        u  = c2g * wenoCoefs;
+        du = c2g * c2d * wenoCoefs;
+      } else {
+        u  = c2g * s2c * stencil;
+        du = c2g * c2d * s2c * stencil;
+      }
       for (int ii=0; ii < ord; ii++) { du(ii) /= dx; }
       SArray<real,2,ord,ord> u_DTs;
       SArray<real,2,ord,ord> du_DTs;
@@ -270,7 +317,7 @@ realHost1d simulate_prim1( int nx , real sim_time ) {
 
 // Primitive equations with boundary terms in central integral using upwind values
 template <int ord>
-realHost1d simulate_prim2( int nx , real sim_time ) {
+realHost1d simulate_prim2( int nx , real sim_time , bool weno ) {
   int constexpr hs = (ord-1)/2;
 
   // Setup transformation matrices and WENO
@@ -288,6 +335,13 @@ realHost1d simulate_prim2( int nx , real sim_time ) {
 
   TransformMatrices::get_gll_points (gllPts_ord);
   TransformMatrices::get_gll_weights(gllWts_ord);
+
+  SArray<real,3,ord,ord,ord> wenoRecon;
+  SArray<real,1,hs+2> idl;
+  real sigma;
+
+  TransformMatrices::weno_sten_to_coefs(wenoRecon);
+  weno::wenoSetIdealSigma<ord>(idl,sigma);
 
   real dx = 1./nx;
   real dt = 0.4 * dx / 1.25;
@@ -318,8 +372,17 @@ realHost1d simulate_prim2( int nx , real sim_time ) {
       SArray<real,1,ord> stencil;
       for (int ii=0; ii < ord; ii++) { stencil(ii) = state(i+ii); }
       // Reconstruct
-      SArray<real,1,ord> u  = c2g       * s2c * stencil;
-      SArray<real,1,ord> du = c2g * c2d * s2c * stencil;
+      SArray<real,1,ord> u ;
+      SArray<real,1,ord> du;
+      if (weno) {
+        SArray<real,1,ord> wenoCoefs;
+        weno::compute_weno_coefs<ord>( wenoRecon , stencil , wenoCoefs , idl , sigma );
+        u  = c2g * wenoCoefs;
+        du = c2g * c2d * wenoCoefs;
+      } else {
+        u  = c2g * s2c * stencil;
+        du = c2g * c2d * s2c * stencil;
+      }
       for (int ii=0; ii < ord; ii++) { du(ii) /= dx; }
       SArray<real,2,ord,ord> u_DTs;
       SArray<real,2,ord,ord> du_DTs;
@@ -358,8 +421,8 @@ realHost1d simulate_prim2( int nx , real sim_time ) {
       fwaves(1,i  ) = u_DTs(0,0    );
       fwaves(0,i+1) = u_DTs(0,ord-1);
 
-      ctend (1,i  ) = -u_du_DTs(0,0    );
-      ctend (0,i+1) = -u_du_DTs(0,ord-1);
+      ctend (1,i  ) = -u_du_DTs(0,0    ) * gllWts_ord(0    );
+      ctend (0,i+1) = -u_du_DTs(0,ord-1) * gllWts_ord(ord-1);
     }
 
     // periodic boundaries
@@ -379,131 +442,8 @@ realHost1d simulate_prim2( int nx , real sim_time ) {
       fwaves(1,i) = u*du;
 
       // Update left and right quadrature using upwind values
-      if (i > 0 ) tend(i-1) += ctend(0,i) * gllWts_ord(0    );
-      if (i < nx) tend(i  ) += ctend(0,i) * gllWts_ord(ord-1);
-    }
-    
-    // Apply tendencies
-    for (int i=0; i < nx; i++) {
-      tend(i) += - (fwaves(1,i) + fwaves(0,i+1)) / dx;
-      state(hs+i) = state(hs+i) + dt * tend(i);
-    }
-    etime += dt;
-  }
-
-  realHost1d state_ret("state_ret",nx);
-  for (int i=0; i < nx; i++) { state_ret(i) = state(hs+i); }
-  return state_ret;
-}
-
-
-
-
-// Primitive equations, except I average out the Jacobian in the central term
-// and pull it out of the integral expression.
-template <int ord>
-realHost1d simulate_prim3( int nx , real sim_time ) {
-  int constexpr hs = (ord-1)/2;
-
-  // Setup transformation matrices and WENO
-  SArray<real,1,ord> gllPts_ord;
-  SArray<real,1,ord> gllWts_ord;
-  SArray<real,2,ord,ord> s2c;
-  SArray<real,2,ord,ord> c2g;
-  SArray<real,2,ord,ord> g2c;
-  SArray<real,2,ord,ord> c2d;
-
-  TransformMatrices::coefs_to_deriv(c2d);
-  TransformMatrices::sten_to_coefs (s2c);
-  TransformMatrices::coefs_to_gll  (c2g);
-  TransformMatrices::gll_to_coefs  (g2c);
-
-  TransformMatrices::get_gll_points (gllPts_ord);
-  TransformMatrices::get_gll_weights(gllWts_ord);
-
-  real dx = 1./nx;
-  real dt = 0.4 * dx / 1.25;
-  realHost1d state ("state" ,nx+2*hs);
-  realHost2d fwaves("fwaves",2,nx+1);
-  realHost1d tend  ("tend"  ,nx);
-  real etime = 0;
-  for (int i=0; i < nx; i++) {
-    state(hs+i) = 0;
-    for (int ii=0; ii < ord; ii++) {
-      real xloc = (i+0.5)*dx + gllPts_ord(ii)*dx;
-      state(hs+i) += func(xloc) * gllWts_ord(ii);
-    }
-  }
-  while (etime < sim_time) {
-    if (sim_time - etime < dt) dt = sim_time - etime;
-
-    // periodic boundaries
-    for (int i=0; i < hs; i++) {
-      state(i) = state(nx+i);
-      state(hs+nx+i) = state(hs+i);
-    }
-
-    // Compute state at cell boundaries
-    for (int i=0; i < nx; i++) {
-      // Form stencil
-      SArray<real,1,ord> stencil;
-      for (int ii=0; ii < ord; ii++) { stencil(ii) = state(i+ii); }
-      // Reconstruct
-      SArray<real,1,ord> u  = c2g       * s2c * stencil;
-      SArray<real,1,ord> du = c2g * c2d * s2c * stencil;
-      for (int ii=0; ii < ord; ii++) { du(ii) /= dx; }
-      SArray<real,2,ord,ord> u_DTs;
-      SArray<real,2,ord,ord> du_DTs;
-      SArray<real,2,ord,ord> u_du_DTs;
-
-      for (int ii=0; ii < ord; ii++) {
-        u_DTs   (0,ii) = u (ii);
-        du_DTs  (0,ii) = du(ii);
-        u_du_DTs(0,ii) = u(ii)*du(ii);
-      }
-
-      // Ader
-      for (int kt=0; kt < ord-1; kt++) {
-        for (int ii=0; ii < ord; ii++) {
-          u_DTs(kt+1,ii) = - u_du_DTs(kt,ii) / (kt+1.);
-        }
-        for (int ii=0; ii < ord; ii++) { u(ii) = u_DTs(kt+1,ii); }
-        du = c2g * c2d * g2c * u;
-        for (int ii=0; ii < ord; ii++) { du_DTs(kt+1,ii) = du(ii)/dx; }
-        for (int ii=0; ii < ord; ii++) {
-          real tot = 0;
-          for (int rt=0; rt <= kt+1; rt++) {
-            tot += u_DTs(rt,ii) * du_DTs(kt+1-rt,ii);
-          }
-          u_du_DTs(kt+1,ii) = tot;
-        }
-      }
-      compute_timeAvg( u_DTs    , dt );
-      compute_timeAvg( u_du_DTs , dt );
-
-      real uavg = 0;
-      for (int ii=0; ii < ord; ii++) {
-        uavg += gllWts_ord(ii) * u_DTs(0,ii);
-      }
-
-      tend(i) = - uavg * (u_DTs(0,ord-1) - u_DTs(0,0)) / dx;
-
-      fwaves(1,i  ) = u_DTs(0,0    );
-      fwaves(0,i+1) = u_DTs(0,ord-1);
-    }
-
-    // periodic boundaries
-    fwaves(0,0 ) = fwaves(0,nx);
-    fwaves(1,nx) = fwaves(1,0 );
-
-    // Compute f-waves
-    for (int i=0; i < nx+1; i++) {
-      real um = fwaves(0,i);
-      real up = fwaves(1,i);
-      real u = 0.5 * (um + up);
-      real du = up - um;
-      fwaves(0,i) = 0;
-      fwaves(1,i) = u*du;
+      if (i > 0 ) tend(i-1) += ctend(0,i);
+      if (i < nx) tend(i  ) += ctend(0,i);
     }
     
     // Apply tendencies
@@ -523,17 +463,17 @@ realHost1d simulate_prim3( int nx , real sim_time ) {
 
 
 int main() {
-  real constexpr sim_time = 0.1;
+  real constexpr sim_time = 0.001;
+  bool constexpr weno = false;
 
-  realHost1d state_hi = simulate_cons<5>( 1000 , sim_time );
+  // realHost1d state_hi = simulate_cons<5>( 1000 , sim_time );
 
-  real l1_100 = L1( simulate_prim3<5>( 100 , sim_time ) , state_hi );
-  real l1_50  = L1( simulate_prim3<5>( 50  , sim_time ) , state_hi );
+  // real l1_100 = L1( simulate_prim1<5>( 100 , sim_time ) , state_hi );
+  // real l1_50  = L1( simulate_prim1<5>( 50  , sim_time ) , state_hi );
 
-  std::cout << log(l1_100 / l1_50) / log(50./100.) << "\n";
-  // print_state( simulate_prim1<5>( 100 , sim_time ) );
+  // std::cout << log(l1_100 / l1_50) / log(50./100.) << "\n";
+  // // print_state( simulate_prim1<5>( 100 , sim_time ) );
 
-  std::cout << L1( simulate_cons <5>( 50 , sim_time ) , state_hi ) << "\n";
-  std::cout << L1( simulate_prim3<5>( 50 , sim_time ) , state_hi ) << "\n";
+  print_states( simulate_cons<9>(100,sim_time,weno) , simulate_prim1<9>(100,sim_time,weno) , simulate_prim2<9>(100,sim_time,weno) );
 }
 
