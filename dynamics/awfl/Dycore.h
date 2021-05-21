63634325d540d95eea97d3d3efe95552b9f279cc
Matt Norman <normanmr@ornl.gov>
2021-05-21 14:43:50 -0400

    Hoping to begin the HEVI work in earnest

--- dynamics/awfl/Dycore.h
+++ dynamics/awfl/Dycore.h
@@ -1,9 +1,10 @@
 
-#include "Spatial_prim_anelastic_fv_Agrid.h"
-#include "Temporal_ssprk3.h"
+#include "Spatial_cons_hevi1_fv_Agrid.h"
+#include "Temporal_ader.h"
 
 // Define the Spatial operator based on constants from the Temporal operatora header file
 typedef Spatial_operator<nTimeDerivs,timeAvg,nAder> Spatial;
 
 // Define the Temporal operator based on the Spatial operator
 typedef Temporal_operator<Spatial> Dycore;
+
