bd916dbaacb19a523b87a29cb96f7fbbba954f37
Matt Norman <normanmr@ornl.gov>
2021-06-30 12:21:32 -0400

    Upgrading SCAMPAM to the new YAKL with easier CMake integration

--- standalone/build/cmakescript.sh
+++ standalone/build/cmakescript.sh
@@ -3,10 +3,14 @@
 ./cmakeclean.sh
 
 cmake      \
-  -DCMAKE_CXX_FLAGS="${CXXFLAGS}"   \
+  -DYAKL_CUDA_FLAGS="${YAKL_CUDA_FLAGS} -DPAM_STANDALONE" \
+  -DYAKL_CXX_FLAGS="${YAKL_CXX_FLAGS} -DPAM_STANDALONE" \
+  -DYAKL_SYCL_FLAGS="${YAKL_SYCL_FLAGS} -DPAM_STANDALONE" \
+  -DYAKL_OPENMP45_FLAGS="${YAKL_OPENMP45_FLAGS} -DPAM_STANDALONE" \
+  -DYAKL_HIP_FLAGS="${YAKL_HIP_FLAGS} -DPAM_STANDALONE" \
   -DNCFLAGS="${NCFLAGS}"            \
   -DYAKL_HOME="${YAKL_HOME}"        \
-  -DARCH="${ARCH}"                  \
+  -DYAKL_ARCH="${YAKL_ARCH}"                  \
   -DPAM_DYCORE="awfl"               \
-  -DPAM_MICRO="none"                \
+  -DPAM_MICRO="kessler"                \
   ..
