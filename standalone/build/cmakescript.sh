5e63866050308d4ade22c8419bb529e34ec8c4d5
Matt Norman <normanmr@ornl.gov>
2021-08-16 12:29:54 -0400

    Updating local machine environment files. Moving to target based YAKL build

--- standalone/build/cmakescript.sh
+++ standalone/build/cmakescript.sh
@@ -6,12 +6,13 @@ cmake      \
   -DYAKL_CUDA_FLAGS="${YAKL_CUDA_FLAGS} -DPAM_STANDALONE"         \
   -DYAKL_CXX_FLAGS="${YAKL_CXX_FLAGS} -DPAM_STANDALONE"           \
   -DYAKL_SYCL_FLAGS="${YAKL_SYCL_FLAGS} -DPAM_STANDALONE"         \
-  -DYAKL_OPENMP_FLAGS="${YAKL_OPENMP_FLAGS} -DPAM_STANDALONE" \
+  -DYAKL_OPENMP_FLAGS="${YAKL_OPENMP_FLAGS} -DPAM_STANDALONE"     \
   -DYAKL_OPENMP45_FLAGS="${YAKL_OPENMP45_FLAGS} -DPAM_STANDALONE" \
   -DYAKL_HIP_FLAGS="${YAKL_HIP_FLAGS} -DPAM_STANDALONE"           \
-  -DNCFLAGS="${NCFLAGS}"            \
-  -DYAKL_HOME="${YAKL_HOME}"        \
-  -DYAKL_ARCH="${YAKL_ARCH}"        \
-  -DPAM_DYCORE="awfl"               \
-  -DPAM_MICRO="p3"                  \
+  -DYAKL_C_FLAGS="${YAKL_C_FLAGS}"                                \
+  -DYAKL_F90_FLAGS="${YAKL_F90_FLAGS}"                            \
+  -DNCFLAGS="${NCFLAGS}"                                          \
+  -DYAKL_ARCH="${YAKL_ARCH}"                                      \
+  -DPAM_DYCORE="awfl"                                             \
+  -DPAM_MICRO="p3"                                                \
   ..
