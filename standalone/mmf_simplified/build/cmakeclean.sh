c3978a446d8973235c2df28598feb78347ba3374
Matt Norman <normanmr@ornl.gov>
2022-01-31 11:10:12 -0500

    Preparing cmakeclean for ekat

--- standalone/mmf_simplified/build/cmakeclean.sh
+++ standalone/mmf_simplified/build/cmakeclean.sh
@@ -2,5 +2,5 @@
 
 rm -rf CMakeCache.txt  CMakeFiles  cmake_install.cmake  CTestTestfile.cmake  Makefile  yakl  driver  \
        exp_drivers  dynamics  micro  scream_common  fort.1  yakl_timer_output.txt  skew.nc  prof.out \
-       test_*.nc  sgs
+       sgs  externals  ekat  a.out
 
