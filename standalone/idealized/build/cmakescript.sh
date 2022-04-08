6ed9d6c6d7830ea961b0662f2eeca7f5a0592692
Matt Norman <normanmr@ornl.gov>
2022-04-08 09:14:54 -0400

    Removing awfl_const.h from all physics since it was never actually needed or used. Adding new accessors to coupler. Changing mmf_simplified driver to use MPI.

--- standalone/idealized/build/cmakescript.sh
+++ standalone/idealized/build/cmakescript.sh
@@ -14,6 +14,6 @@ cmake      \
   -DNCFLAGS="${NCFLAGS}"                                          \
   -DYAKL_ARCH="${YAKL_ARCH}"                                      \
   -DPAM_DYCORE="awfl"                                             \
-  -DPAM_MICRO="kessler"                                           \
-  -DPAM_SGS="none"                                                \
+  -DPAM_MICRO="p3"                                                \
+  -DPAM_SGS="shoc"                                                \
   ..
