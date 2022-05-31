0b989fd1dd5cc8354fc46c43ea0602d6f1af2135
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2022-05-31 17:18:02 -0600

    Add cwD1 and cwDbar2 operators

--- dynamics/spam/src/operators/ext_deriv.h
+++ dynamics/spam/src/operators/ext_deriv.h
@@ -3,6 +3,54 @@
 #include "common.h"
 
 template <uint ndofs>
+void YAKL_INLINE cwDbar2(SArray<real, 1, ndofs> &var, real c,
+                         SArray<real, 2, ndims, 2> const &flux) {
+
+  for (int l = 0; l < ndofs; l++) {
+    var(l) = 0.;
+    for (int d = 0; d < ndims; d++) {
+      var(l) += c * (flux(d, 1) - flux(d, 0));
+    }
+  }
+}
+
+template <uint ndofs, ADD_MODE addmode = ADD_MODE::REPLACE>
+YAKL_INLINE void compute_cwDbar2(real5d tendvar, real c, const real5d U, int is,
+                                 int js, int ks, int i, int j, int k, int n) {
+  SArray<real, 1, ndofs> tend;
+  SArray<real, 3, ndofs, ndims, 2> recon;
+  SArray<real, 2, ndims, 2> flux;
+
+  for (int d = 0; d < ndims; d++) {
+    for (int m = 0; m < 2; m++) {
+      if (d == 0) {
+        flux(d, m) = U(d, k + ks, j + js, i + is + m, n);
+      }
+      if (d == 1) {
+        flux(d, m) = U(d, k + ks, j + js + m, i + is, n);
+      }
+    }
+  }
+
+  cwDbar2<ndofs>(tend, c, flux);
+
+  for (int l = 0; l < ndofs; l++) {
+    tendvar(l, k + ks, j + js, i + is, n) = tend(l);
+  }
+
+  if (addmode == ADD_MODE::REPLACE) {
+    for (int l = 0; l < ndofs; l++) {
+      tendvar(l, k + ks, j + js, i + is, n) = tend(l);
+    }
+  }
+  if (addmode == ADD_MODE::ADD) {
+    for (int l = 0; l < ndofs; l++) {
+      tendvar(l, k + ks, j + js, i + is, n) += tend(l);
+    }
+  }
+}
+
+template <uint ndofs>
 void YAKL_INLINE wDbar2(SArray<real, 1, ndofs> &var,
                         SArray<real, 3, ndofs, ndims, 2> const &recon,
                         SArray<real, 2, ndims, 2> const &flux) {
@@ -146,6 +194,48 @@ YAKL_INLINE void compute_wDvbar_fct(real5d tendvar, const real5d vertreconvar,
   if (addmode == ADD_MODE::ADD) {
     for (int l = 0; l < ndofs; l++) {
       tendvar(l, k + ks, j + js, i + is, n) += tend(l);
+    }
+  }
+}
+
+template <uint ndofs>
+void YAKL_INLINE cwD1(SArray<real, 1, ndims> &var, real c,
+                      SArray<real, 3, ndofs, ndims, 2> const &dens) {
+
+  for (int d = 0; d < ndims; d++) {
+    var(d) = 0.0;
+    for (int l = 0; l < ndofs; l++) {
+      var(d) += c * (dens(l, d, 1) - dens(l, d, 0));
+    }
+  }
+}
+
+template <uint ndofs, ADD_MODE addmode = ADD_MODE::REPLACE>
+void YAKL_INLINE compute_cwD1(real5d tendvar, real c, const real5d densvar,
+                              int is, int js, int ks, int i, int j, int k,
+                              int n) {
+  SArray<real, 1, ndims> tend;
+  SArray<real, 2, ndofs, ndims> recon;
+  SArray<real, 3, ndofs, ndims, 2> dens;
+  for (int l = 0; l < ndofs; l++) {
+    for (int d = 0; d < ndims; d++) {
+      dens(l, d, 1) = densvar(l, k + ks, j + js, i + is, n);
+      if (d == 0) {
+        dens(l, d, 0) = densvar(l, k + ks, j + js, i + is - 1, n);
+      }
+      if (d == 1) {
+        dens(l, d, 0) = densvar(l, k + ks, j + js - 1, i + is, n);
+      }
+      // if (d==2) {dens(l,d,0) = densvar(l,k+ks-1,j+js,i+is);}
+    }
+  }
+  cwD1<ndofs>(tend, c, dens);
+  for (int d = 0; d < ndims; d++) {
+    if (addmode == ADD_MODE::REPLACE) {
+      tendvar(d, k + ks, j + js, i + is, n) = tend(d);
+    }
+    if (addmode == ADD_MODE::ADD) {
+      tendvar(d, k + ks, j + js, i + is, n) += tend(d);
     }
   }
 }
