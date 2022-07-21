adbc9406bd1f892b4e3d7a90d69d331cd319fb1b
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2022-07-21 11:41:14 -0600

    Improve format_spam script

--- utils/format_spam.sh
+++ utils/format_spam.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 script_dir=`dirname "$0"`
-for file in `find $script_dir/../dynamics/spam -name "*.[h|cpp]" `; do 
+for file in `find $script_dir/../dynamics/spam -type f \( -name \*.h -o -name \*.cpp \) -not -path "*/build/*"`; do
         #echo $file
 	clang-format --style=llvm -i $file
 done
