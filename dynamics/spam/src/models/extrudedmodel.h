138ff7d10edb9ada6bb271e6041b381e1fb83fd3
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2022-09-02 13:33:55 -0600

    Functional derivatives

--- dynamics/spam/src/models/extrudedmodel.h
+++ dynamics/spam/src/models/extrudedmodel.h
@@ -384,8 +384,8 @@ public:
         SimpleBounds<4>(primal_topology.ni, primal_topology.n_cells_y,
                         primal_topology.n_cells_x, primal_topology.nens),
         YAKL_LAMBDA(int k, int j, int i, int n) {
-          //Hs.compute_dHsdx<addmode>(Bvar, densvar, HSvar, pis, pjs, pks, i, j,
-          //                          k, n, fac);
+          Hs.compute_dHsdx<addmode>(Bvar, densvar, HSvar, pis, pjs, pks, i, j,
+                                    k, n, fac);
           Hk.compute_dKddens<ADD_MODE::ADD>(Bvar, Kvar, pis, pjs, pks, i, j, k,
                                             n, fac);
         });
@@ -696,15 +696,13 @@ public:
     const auto &Bvar = auxiliary_vars.fields_arr[BVAR].data;
     const auto &HSvar = const_vars.fields_arr[HSVAR].data;
 
+    YAKL_SCOPE(Hk, ::Hk);
+    YAKL_SCOPE(Hs, ::Hs);
     parallel_for(
-        "TEST",
-        SimpleBounds<4>(primal_topology.ni, primal_topology.n_cells_y,
-                        primal_topology.n_cells_x, primal_topology.nens),
+        "Functional derivatives",
+        SimpleBounds<4>(dual_topology.ni, dual_topology.n_cells_y,
+                        dual_topology.n_cells_x, dual_topology.nens),
         YAKL_CLASS_LAMBDA(int k, int j, int i, int n) {
-          
-          //compute_Iext<ndensity, diff_ord, vert_diff_ord>(
-          //    dens0var, densvar, this->primal_geometry, this->dual_geometry,
-          //    pis, pjs, pks, i, j, k, n);
          
           SArray<real, 1, ndensity> dens0_ik;
           SArray<real, 1, ndensity> dens0_im1;
@@ -719,11 +717,6 @@ public:
           compute_Iext<ndensity, diff_ord, vert_diff_ord>(
               dens0_km1, densvar, this->primal_geometry, this->dual_geometry,
               pis, pjs, pks, i, j, k-1, n);
-
-          //compute_Iext<ndensity, diff_ord, vert_diff_ord>(
-          //    dens0var, densvar, this->primal_geometry, this->dual_geometry,
-          //    pis, pjs, pks, i, j, k, n);
-
 
           SArray<real, 1, ndims> u_ik;
           SArray<real, 1, 1> uw_ik;
@@ -758,8 +751,7 @@ public:
           K2 += 0.5_fp * (Wvar(0, k - 1 + pks, j + pjs, i + pis, n) * uw_ik(0) +
                           Wvar(0, k + pks, j + pjs, i + pis, n) * uw_kp1(0));
 
-          //Kvar(0, k + pks, j + pjs, i + pis, n) = 0.5_fp * K2;
-          real K = 0.5_fp * K2;
+          Kvar(0, k + pks, j + pjs, i + pis, n) = 0.5_fp * K2;
 
           if (addmode == ADD_MODE::ADD) {
             Fvar(0, pks + k, pjs + j, pis + i, n) +=  fac * 
@@ -772,12 +764,6 @@ public:
             FWvar(0, pks + k, pjs + j, pis + i, n) = fac *
               0.5_fp * (dens0_ik(0) + dens0_km1(0)) * uw_ik(0);
           }
-
-          //Uvar(0, k + pks, j + pjs, i + pis, n) = u_ik(0);
-          //UWvar(0, k + pks, j + pjs, i + pis, n) = uw_ik(0);
-          //for (int d = 0; d < ndensity; ++d) {
-          //  dens0var(d, pks + k, pjs + j, pis + i, n) = dens0_ik(d);
-          //}
           
           if (addmode == ADD_MODE::ADD) {
             Hs.compute_dHsdx<ADD_MODE::ADD>(Bvar, densvar, HSvar, pis, pjs, pks, i, j,
@@ -787,51 +773,49 @@ public:
                                                 k, n, fac);
           }
 
-          // hack
-          real Igeom =
-              primal_geometry.get_area_00entity(k + pks, j + pjs, i + pis) /
-              dual_geometry.get_area_11entity(k + pks, j + pjs, i + pis);
-          real K0 = Igeom * K;
-          Bvar(0, k + pks, j + pjs, i + pis, n) += fac * K0;
-
-          //Hk.compute_dKddens<ADD_MODE::ADD>(Bvar, Kvar, pis, pjs, pks, i, j, k,
-          //                                  n, fac);
     });
+    auxiliary_vars.exchange({KVAR});
     
+    parallel_for(
+        "Add K to B",
+        SimpleBounds<4>(primal_topology.ni, primal_topology.n_cells_y,
+                        primal_topology.n_cells_x, primal_topology.nens),
+        YAKL_CLASS_LAMBDA(int k, int j, int i, int n) {
+          Hk.compute_dKddens<ADD_MODE::ADD>(Bvar, Kvar, pis, pjs, pks, i, j, k,
+                                            n, fac);
+    });
+    auxiliary_vars.exchange({BVAR, FVAR, FWVAR});
+
+    // old code
+
     //compute_dens0(auxiliary_vars.fields_arr[DENS0VAR].data,
     //              x.fields_arr[DENSVAR].data);
     //compute_U(auxiliary_vars.fields_arr[UVAR].data, x.fields_arr[VVAR].data);
     //compute_UW(auxiliary_vars.fields_arr[UWVAR].data, x.fields_arr[WVAR].data);
-    //auxiliary_vars.fields_arr[UWVAR].set_bnd(0.0);
-    //auxiliary_vars.exchange({UVAR, UWVAR, DENS0VAR});
-    //auxiliary_vars.fields_arr[UWVAR].set_bnd(0.0);
 
+    //auxiliary_vars.fields_arr[UWVAR].set_bnd(0.0);
     //auxiliary_vars.exchange({UVAR, UWVAR, DENS0VAR});
-    //auxiliary_vars.exchange({DENS0VAR});
-    
 
     //if (addmode == ADD_MODE::ADD) {
     //  compute_F_FW_and_K<ADD_MODE::ADD>(
-    //      fac, auxiliary_vars.fields_arr[FVAR2].data,
-    //      auxiliary_vars.fields_arr[FWVAR2].data,
+    //      fac, auxiliary_vars.fields_arr[FVAR].data,
+    //      auxiliary_vars.fields_arr[FWVAR].data,
     //      auxiliary_vars.fields_arr[KVAR].data, x.fields_arr[VVAR].data,
     //      auxiliary_vars.fields_arr[UVAR].data, x.fields_arr[WVAR].data,
     //      auxiliary_vars.fields_arr[UWVAR].data,
     //      auxiliary_vars.fields_arr[DENS0VAR].data);
     //} else if (addmode == ADD_MODE::REPLACE) {
     //  compute_F_FW_and_K<ADD_MODE::REPLACE>(
-    //      fac, auxiliary_vars.fields_arr[FVAR2].data,
-    //      auxiliary_vars.fields_arr[FWVAR2].data,
+    //      fac, auxiliary_vars.fields_arr[FVAR].data,
+    //      auxiliary_vars.fields_arr[FWVAR].data,
     //      auxiliary_vars.fields_arr[KVAR].data, x.fields_arr[VVAR].data,
     //      auxiliary_vars.fields_arr[UVAR].data, x.fields_arr[WVAR].data,
     //      auxiliary_vars.fields_arr[UWVAR].data,
     //      auxiliary_vars.fields_arr[DENS0VAR].data);
     //}
 
-
-    auxiliary_vars.fields_arr[FWVAR].set_bnd(0.0);
+    //auxiliary_vars.fields_arr[FWVAR].set_bnd(0.0);
     //auxiliary_vars.exchange({FVAR, FWVAR, KVAR});
-    auxiliary_vars.exchange({FVAR, FWVAR});
 
     //if (addmode == ADD_MODE::ADD) {
     //  compute_B<ADD_MODE::ADD>(fac, auxiliary_vars.fields_arr[BVAR].data,
@@ -844,7 +828,7 @@ public:
     //                               x.fields_arr[DENSVAR].data,
     //                               const_vars.fields_arr[HSVAR].data);
     //}
-    auxiliary_vars.exchange({BVAR});
+    //auxiliary_vars.exchange({BVAR});
   }
 
   void apply_symplectic(real dt, FieldSet<nconstant> &const_vars,
