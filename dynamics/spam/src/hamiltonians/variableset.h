1f8202791f4206b4420217e358927a44376a7f45
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2023-02-03 17:03:44 -0700

    Enable coupling to physics in moist anelastic

--- dynamics/spam/src/hamiltonians/variableset.h
+++ dynamics/spam/src/hamiltonians/variableset.h
@@ -22,8 +22,7 @@ struct VS_AN {
   static constexpr bool couple = false;
 };
 struct VS_MAN {
-  // static constexpr bool couple = true; TODO
-  static constexpr bool couple = false;
+  static constexpr bool couple = true;
 };
 struct VS_MCE_rho {
   static constexpr bool couple = true;
