28086a0b45d3653fb28b3c18d8b7f0ef620a243e
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2023-02-17 13:53:14 -0700

    Add dycore.pre_time_loop to idealized driver

--- standalone/idealized/driver.cpp
+++ standalone/idealized/driver.cpp
@@ -119,6 +119,11 @@ int main(int argc, char** argv) {
     }
     #endif
 
+    yakl::timer_start("dycore");
+    // this sets up initial conditons and reference state 
+    dycore.pre_time_loop(coupler);
+    yakl::timer_stop("dycore");
+
     // Now that we have an initial state, define hydrostasis for each ensemble member
     if (use_coupler_hydrostasis) coupler.update_hydrostasis( );
 
