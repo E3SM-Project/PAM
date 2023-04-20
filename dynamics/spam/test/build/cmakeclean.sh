841ef9d568ce8f2565d6c81f4f573c44593eea6e
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2023-04-20 10:28:48 -0600

    Add cmake scripts for building PAM-C unit tests

---
+++ dynamics/spam/test/build/cmakeclean.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+rm -rf CMakeCache.txt  CMakeFiles  Testing cmake_install.cmake  CTestTestfile.cmake  Makefile \
+       yakl pam_core dycore hodge_star operator_properties
+
---
+++ dynamics/spam/test/build/cmakescript.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -x
+
+./cmakeclean.sh
+
+cmake      \
+  -DCMAKE_CXX_COMPILER=${CXX}                    \
+  -DCMAKE_CUDA_HOST_COMPILER=${CXX}              \
+  -DYAKL_CUDA_FLAGS="${YAKL_CUDA_FLAGS}"         \
+  -DYAKL_CXX_FLAGS="${YAKL_CXX_FLAGS}"           \
+  -DYAKL_SYCL_FLAGS="${YAKL_SYCL_FLAGS}"         \
+  -DYAKL_OPENMP_FLAGS="${YAKL_OPENMP_FLAGS}"     \
+  -DYAKL_HIP_FLAGS="${YAKL_HIP_FLAGS}"           \
+  -DYAKL_F90_FLAGS="${YAKL_F90_FLAGS}"           \
+  -DPAM_LINK_FLAGS="${PAM_LINK_FLAGS}"           \
+  -DYAKL_ARCH="${YAKL_ARCH}"                     \
+  ..
