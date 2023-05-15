0900fdcd627288e2bc648afc8c2ab5d1d5c749f8
Maciej Waruszewski <mwarusz@igf.fuw.edu.pl>
2023-05-15 17:08:17 -0600

    Changes to Functional_PVPE_extruded for 3d

--- dynamics/spam/src/hamiltonians/functionals.h
+++ dynamics/spam/src/hamiltonians/functionals.h
@@ -94,99 +94,6 @@ public:
   }
 };
 
-//
-// class Functional_PVPE_rhod {
-// public:
-//   bool is_initialized;
-//
-//    Functional_PVPE_rhod() {
-//      this->is_initialized = false;
-// }
-//
-// void initialize(Parameters &params)
-// {
-//   this->is_initialized = true;
-// }
-//
-// real YAKL_INLINE compute_hv(const real5d& dens, const real5d& densfct, int
-// is, int js, int ks, int i, int j, int k)
-// {
-//   SArray<real,1> hv;
-//   SArray<real,4> Dv;
-//
-//   // compute hv = R h
-//   // Uses linearity of R
-//   Dv(0) = dens(0, k+ks, j+js, i+is) + densfct(0, k+ks, j+js, i+is) +
-//   densfct(1, k+ks, j+js, i+is) + densfct(2, k+ks, j+js, i+is); Dv(1) =
-//   dens(0, k+ks, j+js, i+is-1) + densfct(0, k+ks, j+js, i+is-1) + densfct(1,
-//   k+ks, j+js, i+is-1) + densfct(2, k+ks, j+js, i+is-1); Dv(2) = dens(0, k+ks,
-//   j+js-1, i+is) + densfct(0, k+ks, j+js-1, i+is) + densfct(1, k+ks, j+js-1,
-//   i+is) + densfct(2, k+ks, j+js-1, i+is); Dv(3) = dens(0, k+ks, j+js-1,
-//   i+is-1) + densfct(0, k+ks, j+js-1, i+is-1) + densfct(1, k+ks, j+js-1,
-//   i+is-1) + densfct(2, k+ks, j+js-1, i+is-1); R(hv, Dv);
-//
-//   return hv(0);
-// }
-//
-// real YAKL_INLINE compute_zeta(const real5d& v, int is, int js, int ks, int i,
-// int j, int k)
-// {
-//   SArray<real,1> zeta;
-//   // compute zeta = D2 v
-//   compute_D2<1>(zeta, v, is, js, ks, i, j, k);
-//   return zeta(0);
-// }
-//
-// real YAKL_INLINE compute_eta(const real5d& v, const real5d& coriolis, int is,
-// int js, int ks, int i, int j, int k)
-// {
-//   real zeta = compute_zeta(v, is, js, ks, i, j, k);
-//   return zeta + coriolis(0, k+ks, j+js, i+is);
-// }
-//
-//
-// void YAKL_INLINE compute_q0f0(const real5d& q0, const real5d& f0, const
-// real5d& v, const const real5d& dens, const real5d& densfct, const real5d&
-// coriolis, int is, int js, int ks, int i, int j, int k)
-// {
-//
-//   real hv = compute_hv(dens, densfct, is, js, ks, i, j, k);
-//   real zeta = compute_zeta(v, is, js, ks, i, j, k);
-//
-//   // compute q0 = zeta / hv and f0 = f / hv
-//     q0(0, k+ks, j+js, i+is) = zeta / hv;
-//     f0(0, k+ks, j+js, i+is) = coriolis(0, k+ks, j+js, i+is) / hv;
-//
-// }
-//
-// void YAKL_INLINE compute_q0(const real5d& q0, const real5d& v, const real5d&
-// dens, const real5d& densfct, int is, int js, int ks, int i, int j, int k)
-// {
-// real hv = compute_hv(dens, densfct, is, js, ks, i, j, k);
-// real zeta = compute_zeta(v, is, js, ks, i, j, k);
-// // compute q0 = zeta / hv and f0 = f / hv
-//   q0(0, k+ks, j+js, i+is) = zeta / hv;
-// }
-//
-// pvpe YAKL_INLINE compute_PVPE(const real5d& v, const real5d& dens, const
-// real5d& densfct, const real5d& coriolis, int is, int js, int ks, int i, int
-// j, int k)
-// {
-//   pvpe vals;
-//   real eta = compute_eta(v, coriolis, is, js, ks, i, j, k);
-//   real hv = compute_hv(dens, densfct, is, js, ks, i, j, k);
-//   real q0 = eta / hv;
-//
-//   vals.pv = eta;
-//   vals.pe = 0.5 * eta * q0;
-//
-//   return vals;
-// }
-//
-// };
-//
-//
-
 class Functional_PVPE_extruded {
 
 public:
@@ -252,146 +159,285 @@ public:
     return hv(0);
   }
 
-  real YAKL_INLINE compute_zetaxz(const real5d &v, const real5d &w, int is,
+  real YAKL_INLINE compute_hvyz(const real5d &dens, int is, int js, int ks,
+                                int i, int j, int k, int n) const {
+    SArray<real, 1, 1> hv;
+    SArray<real, 1, 4> Dv;
+
+    // compute hv = R h
+    // Uses linearity of R
+    Dv(0) = varset.get_total_density(dens, k, j, i, ks, js, is, n);
+    Dv(1) = varset.get_total_density(dens, k, j - 1, i, ks, js, is, n);
+    Dv(2) = varset.get_total_density(dens, k + 1, j, i, ks, js, is, n);
+    Dv(3) = varset.get_total_density(dens, k + 1, j - 1, i, ks, js, is, n);
+    R(hv, Dv);
+
+    return hv(0);
+  }
+
+  real YAKL_INLINE compute_hvyz_top(const real5d &dens, int is, int js, int ks,
+                                    int i, int j, int k, int n) const {
+    SArray<real, 1, 1> hv;
+    SArray<real, 1, 4> Dv;
+
+    // compute hv = R h
+    // Uses linearity of R
+    Dv(0) = varset.get_total_density(dens, k, j, i, ks, js, is, n);
+    Dv(1) = varset.get_total_density(dens, k, j - 1, i, ks, js, is, n);
+    Dv(2) =
+        varset.get_total_density(dens, k + 1, j, i, ks, js, is, n); // gets 1/2
+    Dv(3) = varset.get_total_density(dens, k + 1, j - 1, i, ks, js, is,
+                                     n); // gets 1/2
+    Rbnd(hv, Dv);
+
+    return hv(0);
+  }
+
+  real YAKL_INLINE compute_hvyz_bottom(const real5d &dens, int is, int js,
+                                       int ks, int i, int j, int k,
+                                       int n) const {
+    SArray<real, 1, 1> hv;
+    SArray<real, 1, 4> Dv;
+
+    // compute hv = R h
+    // Uses linearity of R
+    Dv(0) = varset.get_total_density(dens, k + 1, j, i, ks, js, is, n);
+    Dv(1) = varset.get_total_density(dens, k + 1, j - 1, i, ks, js, is, n);
+    Dv(2) = varset.get_total_density(dens, k, j, i, ks, js, is, n); // gets 1/2
+    Dv(3) =
+        varset.get_total_density(dens, k, j - 1, i, ks, js, is, n); // gets 1/2
+    Rbnd(hv, Dv);
+
+    return hv(0);
+  }
+
+  real YAKL_INLINE compute_hvxy(const real5d &dens, int is, int js, int ks,
+                                int i, int j, int k, int n) const {
+    SArray<real, 1, 1> hv;
+    SArray<real, 1, 4> Dv;
+
+    // compute hv = R h
+    // Uses linearity of R
+    Dv(0) = varset.get_total_density(dens, k, j, i, ks, js, is, n);
+    Dv(1) = varset.get_total_density(dens, k, j, i - 1, ks, js, is, n);
+    Dv(2) = varset.get_total_density(dens, k, j - 1, i, ks, js, is, n);
+    Dv(3) = varset.get_total_density(dens, k, j - 1, i - 1, ks, js, is, n);
+    R(hv, Dv);
+
+    return hv(0);
+  }
+
+  void YAKL_INLINE compute_zetahz(SArray<real, 1, ndims> &zetahz,
+                                  const real5d &v, const real5d &w, int is,
                                   int js, int ks, int i, int j, int k,
                                   int n) const {
     SArray<real, 2, 1, ndims> zeta;
     // compute zeta = D1_ext "v"
     compute_D1_ext<1>(zeta, v, w, is, js, ks, i, j, k, n);
-    return zeta(0, 0);
+    for (int d = 0; d < ndims; ++d) {
+      zetahz(d) = zeta(0, d);
+    }
   }
 
-  real YAKL_INLINE compute_etaxz(const real5d &v, const real5d &w,
-                                 const real5d &coriolisxz, int is, int js,
-                                 int ks, int i, int j, int k, int n) const {
-    real zeta = compute_zetaxz(v, w, is, js, ks, i, j, k, n);
-    return zeta + coriolisxz(0, k + ks, j + js, i + is, n);
+  void YAKL_INLINE compute_etahz(SArray<real, 1, ndims> &etahz, const real5d &v,
+                                 const real5d &w, const real5d &coriolishz,
+                                 int is, int js, int ks, int i, int j, int k,
+                                 int n) const {
+    SArray<real, 1, ndims> zetahz;
+    compute_zetahz(zetahz, v, w, is, js, ks, i, j, k, n);
+    for (int d = 0; d < ndims; ++d) {
+      etahz(d) = zetahz(d) + coriolishz(d, k + ks, j + js, i + is, n);
+    }
   }
 
-  // This computes true qxz
+  // This computes true qhz
   void YAKL_INLINE compute_qhz(const real5d &qhz, const real5d &v,
                                const real5d &w, const real5d &dens,
-                               const real5d &coriolisxz, int is, int js, int ks,
+                               const real5d &coriolishz, int is, int js, int ks,
                                int i, int j, int k, int n) const {
     // Need to subtract 1 here since d00(i,k) corresponds to p11(i,k)
-    real hv = compute_hvxz(dens, is, js, ks, i, j, k - 1, n);
-    real eta = compute_etaxz(v, w, coriolisxz, is, js, ks, i, j, k - 1, n);
+    SArray<real, 1, ndims> hv;
+    hv(0) = compute_hvxz(dens, is, js, ks, i, j, k - 1, n);
+    if (ndims > 1) {
+      hv(1) = compute_hvyz(dens, is, js, ks, i, j, k - 1, n);
+    }
+
+    SArray<real, 1, ndims> etahz;
+    compute_etahz(etahz, v, w, coriolishz, is, js, ks, i, j, k - 1, n);
+
     // compute q0 = zeta / hv and f0 = f / hv
-    qhz(0, k + ks, j + js, i + is, n) = eta / hv;
+    for (int d = 0; d < ndims; ++d) {
+      qhz(d, k + ks, j + js, i + is, n) = etahz(d) / hv(d);
+    }
   }
 
-  // This computes true qxz
+  // This computes true qhz
   void YAKL_INLINE compute_qhz_top(const real5d &qhz, const real5d &v,
                                    const real5d &w, const real5d &dens,
-                                   const real5d &coriolisxz, int is, int js,
+                                   const real5d &coriolishz, int is, int js,
                                    int ks, int i, int j, int k, int n) const {
+    SArray<real, 1, ndims> hv;
     // Need to subtract 1 here since d00(i,k) corresponds to p11(i,k)
-    real hv = compute_hvxz_top(dens, is, js, ks, i, j, k - 1, n);
-    real eta = compute_etaxz(v, w, coriolisxz, is, js, ks, i, j, k - 1, n);
+    hv(0) = compute_hvxz_top(dens, is, js, ks, i, j, k - 1, n);
+    if (ndims > 1) {
+      hv(1) = compute_hvyz_top(dens, is, js, ks, i, j, k - 1, n);
+    }
+    SArray<real, 1, ndims> etahz;
+    compute_etahz(etahz, v, w, coriolishz, is, js, ks, i, j, k - 1, n);
+
     // compute q0 = zeta / hv and f0 = f / hv
-    qhz(0, k + ks, j + js, i + is, n) = eta / hv;
+    for (int d = 0; d < ndims; ++d) {
+      qhz(d, k + ks, j + js, i + is, n) = etahz(d) / hv(d);
+    }
   }
 
-  // This computes true qxz
+  // This computes true qhz
   void YAKL_INLINE compute_qhz_bottom(const real5d &qhz, const real5d &v,
                                       const real5d &w, const real5d &dens,
-                                      const real5d &coriolisxz, int is, int js,
+                                      const real5d &coriolishz, int is, int js,
                                       int ks, int i, int j, int k,
                                       int n) const {
+    SArray<real, 1, ndims> hv;
     // Need to subtract 1 here since d00(i,k) corresponds to p11(i,k)
-    real hv = compute_hvxz_bottom(dens, is, js, ks, i, j, k - 1, n);
-    real eta = compute_etaxz(v, w, coriolisxz, is, js, ks, i, j, k - 1, n);
+    hv(0) = compute_hvxz_bottom(dens, is, js, ks, i, j, k - 1, n);
+    if (ndims > 1) {
+      hv(1) = compute_hvyz_bottom(dens, is, js, ks, i, j, k - 1, n);
+    }
+    SArray<real, 1, ndims> etahz;
+    compute_etahz(etahz, v, w, coriolishz, is, js, ks, i, j, k - 1, n);
     // compute q0 = zeta / hv and f0 = f / hv
-    qhz(0, k + ks, j + js, i + is, n) = eta / hv;
+    for (int d = 0; d < ndims; ++d) {
+      qhz(d, k + ks, j + js, i + is, n) = etahz(d) / hv(d);
+    }
   }
 
-  // This computes relative qxz
+  // This computes relative qhz
   void YAKL_INLINE compute_qhzfhz(const real5d &qhz, const real5d &fhz,
                                   const real5d &v, const real5d &w,
-                                  const real5d &dens, const real5d &coriolisxz,
+                                  const real5d &dens, const real5d &coriolishz,
                                   int is, int js, int ks, int i, int j, int k,
                                   int n) const {
+    SArray<real, 1, ndims> hv;
     // Need to subtract 1 here since d00(i,k) corresponds to p11(i,k)
-    real hv = compute_hvxz(dens, is, js, ks, i, j, k - 1, n);
-    real zeta = compute_zetaxz(v, w, is, js, ks, i, j, k - 1, n);
+    hv(0) = compute_hvxz(dens, is, js, ks, i, j, k - 1, n);
+    if (ndims > 1) {
+      hv(1) = compute_hvyz(dens, is, js, ks, i, j, k - 1, n);
+    }
+    SArray<real, 1, ndims> zetahz;
+    compute_zetahz(zetahz, v, w, is, js, ks, i, j, k - 1, n);
     // compute q0 = zeta / hv and f0 = f / hv
-    qhz(0, k + ks, j + js, i + is, n) = zeta / hv;
-    fhz(0, k + ks, j + js, i + is, n) =
-        coriolisxz(0, k + ks, j + js, i + is, n) / hv;
+    for (int d = 0; d < ndims; ++d) {
+      qhz(d, k + ks, j + js, i + is, n) = zetahz(d) / hv(d);
+      fhz(d, k + ks, j + js, i + is, n) =
+          coriolishz(d, k + ks, j + js, i + is, n) / hv(d);
+    }
   }
 
-  // This computes relative qxz
+  // This computes relative qhz
   void YAKL_INLINE compute_qhzfhz_top(const real5d &qhz, const real5d &fhz,
                                       const real5d &v, const real5d &w,
                                       const real5d &dens,
-                                      const real5d &coriolisxz, int is, int js,
+                                      const real5d &coriolishz, int is, int js,
                                       int ks, int i, int j, int k,
                                       int n) const {
+    SArray<real, 1, ndims> hv;
     // Need to subtract 1 here since d00(i,k) corresponds to p11(i,k)
-    real hv = compute_hvxz_top(dens, is, js, ks, i, j, k - 1, n);
-    real zeta = compute_zetaxz(v, w, is, js, ks, i, j, k - 1, n);
+    hv(0) = compute_hvxz_top(dens, is, js, ks, i, j, k - 1, n);
+    if (ndims > 1) {
+      hv(1) = compute_hvyz_top(dens, is, js, ks, i, j, k - 1, n);
+    }
+
+    SArray<real, 1, ndims> zetahz;
+    compute_zetahz(zetahz, v, w, is, js, ks, i, j, k - 1, n);
+
     // compute q0 = zeta / hv and f0 = f / hv
-    qhz(0, k + ks, j + js, i + is, n) = zeta / hv;
-    fhz(0, k + ks, j + js, i + is, n) =
-        coriolisxz(0, k + ks, j + js, i + is, n) / hv;
+    for (int d = 0; d < ndims; ++d) {
+      qhz(d, k + ks, j + js, i + is, n) = zetahz(d) / hv(d);
+      fhz(d, k + ks, j + js, i + is, n) =
+          coriolishz(d, k + ks, j + js, i + is, n) / hv(d);
+    }
   }
 
-  // This computes relative qxz
+  // This computes relative qhz
   void YAKL_INLINE compute_qhzfhz_bottom(const real5d &qhz, const real5d &fhz,
                                          const real5d &v, const real5d &w,
                                          const real5d &dens,
-                                         const real5d &coriolisxz, int is,
+                                         const real5d &coriolishz, int is,
                                          int js, int ks, int i, int j, int k,
                                          int n) const {
+    SArray<real, 1, ndims> hv;
     // Need to subtract 1 here since d00(i,k) corresponds to p11(i,k)
-    real hv = compute_hvxz_bottom(dens, is, js, ks, i, j, k - 1, n);
-    real zeta = compute_zetaxz(v, w, is, js, ks, i, j, k - 1, n);
+    hv(0) = compute_hvxz_bottom(dens, is, js, ks, i, j, k - 1, n);
+    if (ndims > 1) {
+      hv(1) = compute_hvyz_bottom(dens, is, js, ks, i, j, k - 1, n);
+    }
+
+    SArray<real, 1, ndims> zetahz;
+    compute_zetahz(zetahz, v, w, is, js, ks, i, j, k - 1, n);
+
     // compute q0 = zeta / hv and f0 = f / hv
-    qhz(0, k + ks, j + js, i + is, n) = zeta / hv;
-    fhz(0, k + ks, j + js, i + is, n) =
-        coriolisxz(0, k + ks, j + js, i + is, n) / hv;
+    for (int d = 0; d < ndims; ++d) {
+      qhz(d, k + ks, j + js, i + is, n) = zetahz(d) / hv(d);
+      fhz(d, k + ks, j + js, i + is, n) =
+          coriolishz(d, k + ks, j + js, i + is, n) / hv(d);
+    }
+  }
+
+  void YAKL_INLINE compute_qxy0fxy0(const real5d &qxy0, const real5d &fxy0,
+                                    const real5d &v, const real5d &w,
+                                    const real5d &dens,
+                                    const real5d &coriolisxy, int is, int js,
+                                    int ks, int i, int j, int k, int n) const {
+    real hvxy = compute_hvxy(dens, is, js, ks, i, j, k, n);
+
+    SArray<real, 1, 1> zeta;
+    compute_D1<1>(zeta, v, is, js, ks, i, j, k, n);
+    qxy0(0, k + ks, j + js, i + is, n) = zeta(0) / hvxy;
   }
 
   pvpe YAKL_INLINE compute_PVPE(const real5d &v, const real5d &w,
-                                const real5d &dens, const real5d &coriolisxz,
+                                const real5d &dens, const real5d &coriolishz,
                                 int is, int js, int ks, int i, int j, int k,
                                 int n) const {
     pvpe vals;
     // No subtraction here since this is called on primal cells p11
-    real eta = compute_etaxz(v, w, coriolisxz, is, js, ks, i, j, k, n);
+    SArray<real, 1, ndims> etahz;
+    compute_etahz(etahz, v, w, coriolishz, is, js, ks, i, j, k, n);
     real hv = compute_hvxz(dens, is, js, ks, i, j, k, n);
-    real q0 = eta / hv;
-    vals.pv = eta;
-    vals.pe = 0.5_fp * eta * q0;
+    real q0 = etahz(0) / hv;
+    vals.pv = etahz(0);
+    vals.pe = 0.5_fp * etahz(0) * q0;
     return vals;
   }
 
   pvpe YAKL_INLINE compute_PVPE_top(const real5d &v, const real5d &w,
                                     const real5d &dens,
-                                    const real5d &coriolisxz, int is, int js,
+                                    const real5d &coriolishz, int is, int js,
                                     int ks, int i, int j, int k, int n) const {
     pvpe vals;
     // No subtraction here since this is called on primal cells p11
-    real eta = compute_etaxz(v, w, coriolisxz, is, js, ks, i, j, k, n);
+    SArray<real, 1, ndims> etahz;
+    compute_etahz(etahz, v, w, coriolishz, is, js, ks, i, j, k, n);
     real hv = compute_hvxz_top(dens, is, js, ks, i, j, k, n);
-    real q0 = eta / hv;
-    vals.pv = eta;
-    vals.pe = 0.5_fp * eta * q0;
+    real q0 = etahz(0) / hv;
+    vals.pv = etahz(0);
+    vals.pe = 0.5_fp * etahz(0) * q0;
     return vals;
   }
 
   pvpe YAKL_INLINE compute_PVPE_bottom(const real5d &v, const real5d &w,
                                        const real5d &dens,
-                                       const real5d &coriolisxz, int is, int js,
+                                       const real5d &coriolishz, int is, int js,
                                        int ks, int i, int j, int k,
                                        int n) const {
     pvpe vals;
     // No subtraction here since this is called on primal cells p11
-    real eta = compute_etaxz(v, w, coriolisxz, is, js, ks, i, j, k, n);
+    SArray<real, 1, ndims> etahz;
+    compute_etahz(etahz, v, w, coriolishz, is, js, ks, i, j, k, n);
     real hv = compute_hvxz_bottom(dens, is, js, ks, i, j, k, n);
-    real q0 = eta / hv;
-    vals.pv = eta;
-    vals.pe = 0.5_fp * eta * q0;
+    real q0 = etahz(0) / hv;
+    vals.pv = etahz(0);
+    vals.pe = 0.5_fp * etahz(0) * q0;
     return vals;
   }
 };
