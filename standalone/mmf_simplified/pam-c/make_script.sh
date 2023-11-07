484857df64a92cc81e20143cb33c9ecc0b8f71dc
Chris Eldred <celdred@sandia.gov>
2023-11-07 14:18:32 -0700

    fix run scripts

--- standalone/mmf_simplified/pam-c/make_script.sh
+++
@@ -1,4 +0,0 @@
-cd ../build
-source ../../machines/linux_laptop_gnu_mpi_cpu.env
-./cmakescript_pamc.sh ../pam-c/set_pamc_cmakevars.sh
-make -j $1
--- standalone/mmf_simplified/pam-c/set_pamc_cmakevars.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-add_cmake_vars="  -DPAMC_MODEL=extrudedmodel "
-add_cmake_vars+=" -DPAMC_HAMIL=an       "
-add_cmake_vars+=" -DPAMC_THERMO=idealgaspottemp      "
-add_cmake_vars+=" -DPAMC_IO=serial        "
-
-#ce mce_rho an man
-#idealgaspottemp constkappavirpottemp
