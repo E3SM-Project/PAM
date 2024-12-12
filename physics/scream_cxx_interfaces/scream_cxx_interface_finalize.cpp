6b162a8892e788d0a672d519374a3c82a0de2a5b
James Foucar <jgfouca@sandia.gov>
2024-12-12 14:53:35 -0700

    Simplify table setup based on new p3 changes

--- physics/scream_cxx_interfaces/scream_cxx_interface_finalize.cpp
+++ physics/scream_cxx_interfaces/scream_cxx_interface_finalize.cpp
@@ -4,22 +4,10 @@
 
 namespace pam {
 
-  extern scream::p3::Functions<scream::Real,scream::DefaultDevice>::view_1d_table      mu_r_table_vals;
-  extern scream::p3::Functions<scream::Real,scream::DefaultDevice>::view_2d_table      vn_table_vals;
-  extern scream::p3::Functions<scream::Real,scream::DefaultDevice>::view_2d_table      vm_table_vals;
-  extern scream::p3::Functions<scream::Real,scream::DefaultDevice>::view_2d_table      revap_table_vals;
-  extern scream::p3::Functions<scream::Real,scream::DefaultDevice>::view_ice_table     ice_table_vals;
-  extern scream::p3::Functions<scream::Real,scream::DefaultDevice>::view_collect_table collect_table_vals;
-  extern scream::p3::Functions<scream::Real,scream::DefaultDevice>::view_dnu_table     dnu_table_vals;
+  extern scream::p3::Functions<scream::Real,scream::DefaultDevice>::P3LookupTables     lookup_tables;
 
   void deallocate_scream_cxx_globals() {
-    mu_r_table_vals    = scream::p3::Functions<scream::Real,scream::DefaultDevice>::view_1d_table     ();
-    vn_table_vals      = scream::p3::Functions<scream::Real,scream::DefaultDevice>::view_2d_table     ();
-    vm_table_vals      = scream::p3::Functions<scream::Real,scream::DefaultDevice>::view_2d_table     ();
-    revap_table_vals   = scream::p3::Functions<scream::Real,scream::DefaultDevice>::view_2d_table     ();
-    ice_table_vals     = scream::p3::Functions<scream::Real,scream::DefaultDevice>::view_ice_table    ();
-    collect_table_vals = scream::p3::Functions<scream::Real,scream::DefaultDevice>::view_collect_table();
-    dnu_table_vals     = scream::p3::Functions<scream::Real,scream::DefaultDevice>::view_dnu_table    ();
+    lookup_tables = decltype(lookup_tables)();
   }
 
   void call_kokkos_finalize() {
